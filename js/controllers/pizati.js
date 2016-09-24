c25563637db3b6909b0c29b8f98feae6c2e3e925
Daler Asrorov <asrorids@gmail.com>
2016-09-24 00:31:20 -0700

    Added change of text feature

--- js/controllers/pizati.js
+++ js/controllers/pizati.js
@@ -3,9 +3,11 @@ angular
   .module('app')
   .controller('PizatiCtrl', function($http) {
     var vm = this;
+    var textEditorColor = '#002b36';
     vm.test = "Test";
     vm.title = "Pizati";
     vm.currentEssay = "Argumentative Essay";
+    vm.authorEssayDataStructure = null;
 
     var options = {
       debug: 'info',
@@ -86,14 +88,45 @@ angular
       }
     ];
 
+    function getEssay(id, givenArray) {
+      givenArray.forEach(function(essay) {
+        if(essay.id === id) {
+          return essay;
+        }
+      });
+    };
+
+    function inputText(text) {
+      console.log('text to input', text);
+      var textLength = text.length;
+      quill.deleteText(0, textLength);
+      quill.insertText(0, text, {
+        'color': textEditorColor
+      });
+    };
+
     retrieveEssays(essasyApiUrl, function(response) {
-      vm.authrEssayDataStructure = response.data.essays;
+      vm.authorEssayDataStructure = response.data.essays;
     });
 
     // this methodshow buttons, tooltips, and commands
     vm.startTutorial = function() {
       vm.essayStarted = true;
       quill.enable();
+    };
+
+    // shows essay text associated with associated id
+    vm.showRespectveEssay = function(essayId) {
+      console.log('essay id', essayId);
+      var arrayP = vm.authorEssayDataStructure;
+
+      arrayP.forEach(function(element) {
+        if(element.id === essayId) {
+          console.log(element);
+          inputText(element.essay.text);
+        }
+      });
+
     };
 
 });
